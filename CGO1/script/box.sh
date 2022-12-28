9f994f6f4de7238b14bee638f9267c44cb9c8714
htaldo <64457490+htaldo@users.noreply.github.com>
2022-12-27 18:03:46 -0600

    Update box.sh

--- CGO1/script/box.sh
+++ CGO1/script/box.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OD=/home/aldo/pro/falcon/script/output
+OD=/home/arturo/proyectoAF/app2/script/output
 
 echo -e "\e[1m\e[36m>>\e[39m getting gridbox...\033[0m"
 awk -f box.awk $OD/coarse.pdbqt | tee $OD/box.txt
