91a55f036b1ea35c720f2fa99fdd4c3fa1461e3c
htaldo <64457490+htaldo@users.noreply.github.com>
2022-12-27 18:02:14 -0600

    Update dock.sh

--- CGO1/script/dock.sh
+++ CGO1/script/dock.sh
@@ -3,15 +3,9 @@
 WD=/home/arturo/proyectoAF/app2/script
 OD=/home/arturo/proyectoAF/app2/script/output
 VINADIR=/home/arturo/.local/src/autodock_vina_1_1_2_linux_x86/bin
-i=1
-
-awk -f $WD/box.awk $OD/coarse.pdbqt
 
 echo -e "\e[1m\e[36m>>\e[39m docking...\033[0m"
-#[ -f "$file" ] || echo "error: $file is not a file!" && exit
 cd $VINADIR
 ./vina --receptor $OD/receptor.pdbqt --ligand $OD/ligand.pdbqt\
 	   --config $OD/box.txt --exhaustiveness=16 --out $OD/docking.pdbqt \
 	   | tee $OD/scores.txt
-
-#rm ligand.mol2 ligand.pdbqt receptor.pdb receptorH.pdb box.txt receptor.pdbqt
